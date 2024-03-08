935ff6c593da296022931432899aff9b78ebcff8
Aditi Anand <110238534+Aditikhasnis@users.noreply.github.com>
2024-03-08 12:28:43 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 
 int main()
 {
-    std::cout << "Hello, world! this is PES2UG21CS031" << std::endl;
+    std::cout << "Hello, world! this is PES2UG21CS031 HELLO EVERYONE !!!" << std::endl;
     return 0;
 }
